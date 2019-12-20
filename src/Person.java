4bf83fc1890a8e3db5d3fed084a38d10cec36713
Mik Pedersen <45688364+MikPedersen@users.noreply.github.com>
2019-12-20 10:43:06 +0100

    opgave 5

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 /**
  * Developed by András Ács (acsandras@gmail.com)
  * Zealand / www.zealand.dk
@@ -7,34 +9,40 @@
 
 public class Person {
 
-    // TODO Ret nedenstående datatyper
     String navn; // Test Testesen
-    String alder; // 18
+    int alder; // 18
     String by; // Næstved
-    String postNummer; // 4700
+    int postNummer; // 4700
     String vej; // Femøvej
     String husnummer; // 3B
     String nationalitet; // Dansk
-    String fritidsStringeresser; // Snitning, LOL, strikke
-    String smarthomeDevices; // Xbox, 3 Smartlamper, Køleskab m.m.
-    String hojde; // 1.56 m
-    String vaegt;  // 76 kg
-    String karaktergennemsnit; // 5.33
-    String eksamensKarakterer; // 7, 12, -3, 4, 2, 10
-    String opdateretDato; // Sun Jan 08 00:00:00 CET 1978
+    String[] fritidsStringeresser; // Snitning, LOL, strikke
+    String[] smarthomeDevices; // Xbox, 3 Smartlamper, Køleskab m.m.
+    double hojde; // 1.56 m
+    double vaegt;  // 76 kg
+    double karaktergennemsnit; // 5.33
+    int[] eksamensKarakterer; // 7, 12, -3, 4, 2, 10
+    Date opdateretDato; // Sun Jan 08 00:00:00 CET 1978
 
-    // TODO Vis eksempler på typecasting
     @Override
     public String toString() {
         return "Navn: " + navn + "By" + by;
     }
+    int vaegt2 = (int) vaegt;
+    byte hojde2 = (byte) hojde;
+
 
     public static void main(String[] args) {
 
         Person person = new Person();
         person.navn = "Test Testesen";
         person.by = "Næstved";
-        System.out.println();
+        person.vaegt = (long) 670000000;
+        person.husnummer = String.valueOf((int) 32);
+        person.postNummer = Integer.parseInt( "4700");
+        System.out.println(person.husnummer + " " + person.postNummer);
+
+
     }
 
 }
